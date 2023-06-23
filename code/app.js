67f49b7e21f51cb639c066137becb7814231835e
mimsotu <mimisotudeh@gmail.com>
2023-06-23 16:30:49 +0100

    Create cardArray and card objects

---
+++ code/app.js
@@ -0,0 +1,64 @@
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    }
+
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    }
+    
+]
+
+console.log(cardArray);
